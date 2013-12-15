b92d7c3a858bc13b88ae339ffe17662e71d85b26
Akenaide <kevinms1@gmail.com>
2013-12-15 23:40:43 +0100

    Change a loop and use a parser

--- main.py
+++ main.py
@@ -6,37 +6,8 @@ Created on 7 août 2013
 from CharPuush import CharPuush
 import webbrowser
 
-if __name__ == '__main__':
-    pass
 
-entry = input('lien puush :')
-link = entry[14:]
-link = link.split('.')[0]
-idPuush = list()
-continu = 'True'
-
-for charac in link:
-    temp = CharPuush(charac)
-    idPuush.append(temp)
-    
-while continu == 1:
-    parser = myparser()
-
-    n = int(input("nombre de tour : "))
-    
-    for i in range(0, n):
-        i = 1
-        while idPuush[-i].incre():
-            i += 1
-        
-        temp = "".join(x.get_value() for x in idPuush)
-        webbrowser.open_new('http://puu.sh/'+temp)
-    input("Continuer ? : ")
-    continu = input("1. pour continuer")
-    print(continu)
-
-
-def myparser():
+def mkparser():
     import argparse
 
     parser = argparse.ArgumentParser(description='Puush increment')
@@ -44,3 +15,34 @@ def myparser():
     parser.add_argument('number', help='number of link', type=int)
 
     return parser.parse_args()
+
+def main():
+    args = mkparser()
+
+    entry = args.link
+    # entry = input('lien puush :')
+    link = entry[14:]
+    link = link.split('.')[0]
+    idPuush = list()
+    continu = 1
+
+    for charac in link:
+        temp = CharPuush(charac)
+        idPuush.append(temp)
+
+    while continu == 1:
+        #n = int(input("nombre de tour : "))
+        number_iteration = args.number
+        i = 0
+        while i < number_iteration:
+            rank = 1
+
+            while idPuush[-rank].incre():
+                rank += 1
+
+            temp = "".join(x.get_value() for x in idPuush)
+            webbrowser.open_new('http://puu.sh/'+temp)
+        continu = int(input("1. pour continuer"))
+
+if __name__ == '__main__':
+    main()
